9d40d451311e7185f9241d39870504e87ff25bc0
Punnakan Punsiri <punnakan2509@gmail.com>
2024-09-25 00:12:18 +0700

    2commit

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
+//เวลา
 let dt = new Date();
 document.getElementById("datetime").innerHTML = dt.toLocaleString();
+
+const submit_btn = document.getElementById("submit-btn");
+const task_box = document.getElementById("task-box");
+const li = document.createElement("li");
+
+submit_btn.addEventListener("click", function () {
+  if (task_box.textContent === "") {
+    alert("Add your task");
+  } else {
+  }
+});
